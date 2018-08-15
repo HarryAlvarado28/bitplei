3b34e5f678e2c9b6fd92de170d2796149d0d3cc9
harryalvarado <harry2alvarado8@gmail.com>
2018-08-15 15:35:15 -0500

    Se logra tomar el valor del textfield al dar click

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from "@angular/core";
+import { TextField } from "ui/text-field";
+
 
 @Component({
-    selector: "Home",
+    selector: "BITPLEI",
     moduleId: module.id,
     templateUrl: "./home.component.html"
 })
+
 export class HomeComponent implements OnInit {
+    // Texto se imprime en el label del archivo html.
+    public message: string = "Bienvenido!";
 
     constructor() {
         // Use the component constructor to inject providers.
@@ -13,5 +18,13 @@ export class HomeComponent implements OnInit {
 
     ngOnInit(): void {
         // Init your component properties here.
+
     }
+
+    public onButtonTap(args) {
+        console.log('Q Xopaaaaaaaaa Button tapped', args);
+        alert("valor introducido:: "+args)
+    }
+
 }
+
